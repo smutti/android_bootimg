ffa25860684bbc594453ef490f2d539544d20168
Simone Mutti <simone.mutti@unibg.it>
2015-06-16 14:03:10 +0200

    Update README. Make modification optional.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # android_bootimg
+
+Python utility for unpack, modify and repack an android boot image.
+
+The utility support the modification (i.e., overwrite) of the following files:
+- sepolicy
+- file_contexts
+- seapp_contexts
+- property_contexts
+
+## How to use it
+====================
+**from the command line**:
+
+    python bootimg.py -boot path/to/boot.img -out path/to/out_dir
+
+optionally you can specify the files to overwrite:
+
+*overwrite sepolicy*
+    python bootimg.py -boot path/to/boot.img -sepolicy path/to/sepolicy -out path/to/out_dir
+
+*overwrite file_contexts*
+    python bootimg.py -boot path/to/boot.img -sepolicy path/to/file_contexts -out path/to/out_dir
+
+*overwrite seapp_contexts*
+    python bootimg.py -boot path/to/boot.img -sepolicy path/to/seapp_contexts -out path/to/out_dir
+
+*overwrite property_contexts*
+    python bootimg.py -boot path/to/boot.img -sepolicy path/to/property_contexts -out path/to/out_dir
+
+
+This is a work in progress.  Please give feedback!
--- bootimg.py
+++ bootimg.py
@@ -22,13 +22,26 @@ def repack_boot(base_dir, tmp_dir, out):
     p = subprocess.Popen([mkboot, tmp_dir, join(out, 'newboot.img')])
     p.communicate()
 
-def unpack_repack(bootimg, base_dir, tmp_dir, out, sepolicy):
+def unpack_repack(bootimg, base_dir, tmp_dir, out, sepolicy, file_contexts, seapp_contexts, property_contexts):
 
     ramdisk_dir = unpack_boot(bootimg, base_dir, tmp_dir)
     
-    #copy sepolicy
-    copy(sepolicy, ramdisk_dir)
+    if sepolicy and isfile(sepolicy):
+        print 'Copying sepolicy'
+        copy(sepolicy, ramdisk_dir)
 
+    if file_contexts and isfile(file_contexts):
+        print 'Copying file_contexts'
+        copy(file_contexts, ramdisk_dir)
+
+    if seapp_contexts and isfile(seapp_contexts):
+        print 'Copying seapp_contexts file'
+        copy(seapp_contexts, ramdisk_dir)
+
+    if property_contexts and isfile(property_contexts):
+        print 'Copying property_contexts file'
+        copy(property_contexts, ramdisk_dir)
+        
     repack_boot(base_dir, tmp_dir, out)
 
 
@@ -45,7 +58,12 @@ def update_boot(args):
     
     makedirs(args.out_dir)
 
-    unpack_repack(args.bootimg, project_dir, tmp_dir, args.out_dir, args.sepolicy)
+    if not isfile(args.bootimg):
+        print 'Please specify a valid boot.img file'
+        raise SystemExit
+
+    unpack_repack(args.bootimg, project_dir, tmp_dir, args.out_dir, args.sepolicy,
+                  args.file_contexts, args.seapp_contexts, args.property_contexts)
     rmtree(tmp_dir)
 
 def main():
@@ -53,7 +71,7 @@ def main():
 
     parser = argparse.ArgumentParser(description='Unpack and repack boot.img')
     parser.add_argument('-boot', required=True, dest='bootimg', help='boot.img file')
-    parser.add_argument('-sepolicy', required=True, dest='sepolicy', 
+    parser.add_argument('-sepolicy', required=False, dest='sepolicy', 
                         help='new sepolicy file to add in the boot.img')
     parser.add_argument('-file_contexts', required=False, dest='file_contexts', 
                         help='new file_contexts file to add in the boot.img')
